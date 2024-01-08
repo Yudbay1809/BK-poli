2ba156ede22fe22baa830004a9f99668c8b2398c
Yudha Bayu Prastyo <88624834+Yudbay1809@users.noreply.github.com>
2024-01-08 10:53:51 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -6,7 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sistem Informasi Poliklinik</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
+  
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
     <!-- Font Awesome -->
@@ -35,30 +37,72 @@
 
 <body class="hold-transition sidebar-mini layout-fixed">
     <header class="bg-dark text-white text-center py-5">
-        <h1 class="display-4">Sistem Informasi Poliklinik</h1>
-        <p class="lead">Inovatif, Efisien, dan Berkualitas</p>
+        <h1 class="display-4">BK POLIKLINIK</h1>
+        <p class="lead">Sebatas Capstone BK</p>
     </header>
 
     <section class="container my-5">
-        <div class="row">
-            <div class="col-md-6">
-                <h2>Selamat Datang</h2>
-                <p>
-                    Sistem Informasi Poliklinik membantu Anda mengelola data pasien, informasi obat, dan operasional
-                    klinik dengan lebih efisien. Kami menyediakan solusi yang terintegrasi untuk mendukung kegiatan sehari-hari Anda.
-                </p>
-            </div>
-            <div class="col-md-6">
-                <div class="login-section">
-                    <h2 class="mb-4">Login</h2>
-                    <p>Pilih peran Anda:</p>
-                    <a href="admin/login.php" class="btn btn-dark btn-login">Admin</a>
-                    <a href="dokter/login.php" class="btn btn-dark btn-login">Dokter</a>
-                    <a href="#" class="btn btn-dark btn-login">Pasien</a>
-                </div>
-            </div>
+    <div id="carouselExampleControlsNoTouching" class="carousel slide" data-bs-touch="false">
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img class="bd-placeholder-img bd-placeholder-img-lg d-block w-100" width="800" height="400" src="picture/alat.jpg" role="img" aria-label="Placeholder: First slide" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"></rect></img>
         </div>
+        <div class="carousel-item">
+          <img class="bd-placeholder-img bd-placeholder-img-lg d-block w-100" width="800" height="400" src="picture/ruangan.jpg" role="img" aria-label="Placeholder: Second slide" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#666"></rect></img>
+        </div>
+        <div class="carousel-item">
+          <img class="bd-placeholder-img bd-placeholder-img-lg d-block w-100" width="800" height="400" src="picture/rs.jpg" role="img" aria-label="Placeholder: Third slide" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#555"></rect></img>
+        </div>
+      </div>
+      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControlsNoTouching" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControlsNoTouching" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </button>
+    </div>
     </section>
+
+    <div class="row row-cols-1 row-cols-md-3 g-4">  
+  <div class="col">
+    <div class="card h-100">
+    <img src="picture/admin_profil.png" class="rounded-circle" alt="Cinque Terre" width="304" height="236">
+      <div class="card-body">
+        <h5 class="card-title">Admin</h5>
+        <p class="card-text">Selamat Datang Silahkan Login Terlebih dahulu</p>
+      </div>
+      <div class="card-footer">
+        <a href="admin/login.php" class="btn btn-primary" role="button" aria-disabled="true">Login</a>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+    <img src="picture/admin_profil.png" class="rounded-circle" alt="Cinque Terre" width="304" height="236">
+      <div class="card-body">
+        <h5 class="card-title">Dokter</h5>
+        <p class="card-text">Selamat Datang Silahkan Login Terlebih dahulu.</p>
+      </div>
+      <div class="card-footer">
+      <a href="dokter/login.php" class="btn btn-primary" role="button" aria-disabled="true">Login</a>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+    <img src="picture/admin_profil.png" class="rounded-circle" alt="Cinque Terre" width="304" height="236">
+      <div class="card-body">
+        <h5 class="card-title">Pasien</h5>
+        <p class="card-text">Selamat Datang Silahkan Login Terlebih dahulu</p>
+      </div>
+      <div class="card-footer">
+      <a href="pasien/login.php" class="btn btn-primary" role="button" aria-disabled="true">Login</a>
+      </div>
+    </div>
+  </div>
+</div>
 <!-- jQuery -->
 <script src="plugins/jquery/jquery.min.js"></script>
 <!-- jQuery UI 1.11.4 -->
