ad2cc80872291ff1f094e9f43a8f462d5434eeb8
Yudbay1809 <prastyodx2@gmail.com>
2026-03-30 04:33:26 +0700

    fix(next): exclude client-only from trace

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -8,6 +8,12 @@ const __dirname = path.dirname(__filename);
 const nextConfig = {
   typedRoutes: true,
   outputFileTracingRoot: path.join(__dirname, "../../"),
+  outputFileTracingExcludes: {
+    "*": ["**/node_modules/client-only/**"],
+  },
+  experimental: {
+    outputFileTracingRoot: path.join(__dirname, "../../"),
+  },
 };
 
 export default nextConfig;
